1faa610c9de6f170fa84429be2e3328a58795cea
onnik <onni.kivinen@gmail.com>
2025-04-14 11:43:06 +0300

    fix: correct useMedia hook invocation in Home component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,10 @@
 import MediaRow from '../components/MediaRow';
 import SingleView from '../components/SingleView';
-import {useMedia} from '../hooks/apiHooks';
 import {useState} from 'react';
+import useMedia from '../hooks/apiHooks';
 
 const Home = () => {
-  const mediaArray = useMedia;
+  const mediaArray = useMedia();
   const [selectedItem, setSelectedItem] = useState(null);
 
   return (
